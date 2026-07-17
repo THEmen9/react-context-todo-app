169656c1ebd8fde2822785b598a67f35992604d3
Ankit Kumar <122370470+THEmen9@users.noreply.github.com>
2026-07-17 21:32:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ function App() {
     };
 
     const updateTodo = (id, todo) => {
-        setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+        setTodos((prev) => prev.map((prevTodo)
+         => (prevTodo.id === id ? todo : prevTodo)))
     };
 
     const deleteTodo = (id) => {
